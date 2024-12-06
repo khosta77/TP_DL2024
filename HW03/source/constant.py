fdbfa041bcfe9dda17d745bcdf978a2dcf6812f3
khosta77 <puwerfulpants@gmail.com>
2024-12-06 23:05:10 +0300

    Вынес в константы cir и optim

--- HW03/source/constant.py
+++ HW03/source/constant.py
@@ -1,3 +1,8 @@
+import torch.nn as nn
+import torch.optim as optim
+
 SHAPE = 256
 EPOCH = 40
 LEARNING_RATE = 0.01
+CRITERION = nn.BCELoss()
+OPTIMIZER = lambda m: optim.SGD(m.parameters(), lr=LEARNING_RATE)
